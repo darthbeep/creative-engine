aa575813d9e0109ea2968b566f41c01bfab3b95e
mhreid <micahhelenreid@gmail.com>
2019-10-19 14:41:07 -0400

    Switched app

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 from markov import Markov
-
+from scraper import Scraper
 @app.route('/')
 def index():
-    return render_template('index.html', caption = "")
+    return render_template('index2.html', caption = "")
 
 @app.route('/caption', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        text = request.form['caption']
+        username = request.form['caption']
+        start = request.form['start']
+        end = request.form['end']
     else:
-        text = ""
+        return render_template('index2.html', caption = "Invalid input")
+    scraper = Scraper(username, start, end)
+    text = scraper.text
     try:
         markov = Markov(text)
     except:
-        return render_template('index.html', caption = "Invalid input")
+        return render_template('index2.html', caption = "Invalid input")
     text = markov.generate()
     if text == None:
         text = "Needs more input"
-    return render_template('index.html', caption = text)
+    return render_template('index2.html', caption = text)
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
