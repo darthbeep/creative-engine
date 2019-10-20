bd34889918515524f4aa114d352265c123cb72e4
darthbeep <darthbeep@gmail.com>
2019-10-19 20:06:15 -0400

    minor aesthetic changes

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 from markov import Markov
 from scraper import Scraper
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', caption = "")
+    if request.method == 'GET':
+        return render_template('index.html', caption = "")
+    return submit()
 @app.route('/instructions')
 def instructions():
     return render_template('instructions.html', caption = "")
